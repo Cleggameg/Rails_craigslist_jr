9f2fb817da54620e3075ba2ef92b32bb23ed87be
Chase Nutile & Paul Clegg <pair+b0xw00d.cleggameg@devbootcamp.com>
2015-04-06 15:41:55 -0500

    remove EDIT and DELETE for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,32 +12,12 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
-
   def create
-    @category = Category.new(category_params)
-    if @category.save
+    if @category = Category.find_or_create_by(category_params)
       redirect_to @category
     else
       render 'new'
     end
-  end
-
-  def update
-    @category = Category.find(params[:id])
-    if @category.update(category_params)
-      redirect_to @category
-    else
-      render "edit"
-    end
-  end
-
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
-    redirect_to categories_path
   end
 
   private
